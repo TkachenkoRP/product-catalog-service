ee2ae8936d18865a2cee810c4bcdc08ca848704e
Rodion Tkachenko <tkachenko_rp@mail.ru>
2025-11-26 18:39:05 +0700

    Добавил тестов

--- src/main/java/com/my/service/AuditService.java
+++
@@ -1,8 +0,0 @@
-package com.my.service;
-
-import com.my.model.AuditLog;
-
-public interface AuditService {
-    void logAction(AuditLog.AuditActions action, String details);
-    void showAuditLog();
-}
